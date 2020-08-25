b4abe67b739c32a22198a10161a97c6ca7d3c25a
uden-lang <afiff713@gmail.com>
2020-08-25 16:18:31 +0700

    change text

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
   <h2>PHP</h2>
   <h2>PHP</h2>
   <h2>PHP</h2>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas asperiores accusamus nisi sequi aliquam ad eos labore alias autem, dolor officia atque quas nobis maximedfdf laudantium ut reprehenderit delectus incidunt.</p>
+  <p>Lorem  dolor sit, amet consectetur adipisicing elit. Voluptas asperiores accusamus nisi sequi aliquam ad eos labore alias autem, dolor officia atque quas nobis maximedfdf laudantium ut reprehenderit delectus incidunt.</p>
 </body>
 </html>
