89e62a40363320c1395bc881cba6db432da06e16
uden-lang <afiff713@gmail.com>
2020-08-26 11:38:39 +0700

    add p

--- index.php
+++ index.php
@@ -26,6 +26,9 @@
   <div class="content">
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, qui.</p>
   </div>
+  <div class="content">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, qui.</p>
+  </div>
   <p>Lorem  dolor sit, amet consectetur adipisicing elit. Voluptas asperiores accusamus nisi sequi aliquam ad eos labore alias autem, dolor officia atque quas nobis maximedfdf laudantium ut reprehenderit delectus incidunt.</p>
 </body>
 </html>
